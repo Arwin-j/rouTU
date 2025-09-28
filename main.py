3adcef383cab23a21ec28e616a790e77a1818c0c
Arwin-j <tuq56068@temple.edu>
2025-09-27 21:20:39 -0400

    Adding main fast app

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_root():
+    return FileResponse(os.path.join("static", "index.html"))
+
+@app.get("/input")
+def show_input():
+    return FileResponse("input.html")
